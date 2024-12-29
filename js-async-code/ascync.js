7e566f3fe727710756f4a29b7580ae7be404d473
MIHIR KUMAR <mihirkumar1208@gmail.com>
2024-12-29 14:45:10 +0530

    js-ascynccode;

---
+++ js-async-code/ascync.js
@@ -0,0 +1,30 @@
+//javascript is a synchronous code means phle 1 code chlega uske badd dusra then teesra and so on....
+//it is a single threaded
+
+/*************   execution context   ***********/
+
+   /*
+   execution context means each operation run when last one get completed
+   
+
+   it contians two thing memory heap and call stack
+   
+   
+   
+   
+   */
+
+
+   //blocking code and non-blocking code
+
+   /*
+
+
+   blocking code blocks the flow of program
+   it reads file sync
+
+
+   non-blocking code does not blocks the execution of code
+    it does not read async
+   
+   */
